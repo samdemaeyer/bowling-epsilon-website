4f5609c501b214326d83c81f1e7fbaedcf5129e2
Sam De Maeyer <sam@showmyhomework.co.uk>
2018-07-01 16:52:08 +0100

    Prepare for deploy

--- src/components/Homepage/HomepageCarousel.jsx
+++ src/components/Homepage/HomepageCarousel.jsx
@@ -5,14 +5,26 @@ import sliderSettings from '../../settings';
 // TODO: name `alt`
 const HomepageCarousel = () => (
   <Slider {...sliderSettings()}>
-    <div>
+    <div className="slide-wrapper">
       <img src="images/slider-1.jpg" className="img-responsive" alt="..." />
+      <div className="slide-content">
+        <h3>Bowling, Fun & Food</h3>
+        <h4>come get your game on today</h4>
+      </div>
     </div>
-    <div>
+    <div className="slide-wrapper">
       <img src="images/slider-2.jpg" className="img-responsive" alt="..." />
+      <div className="slide-content">
+        <h3>Bowlers do it in alleys.</h3>
+        <h4>We don’t remember days…we remember moments.</h4>
+      </div>
     </div>
-    <div>
+    <div className="slide-wrapper">
       <img src="images/slider-3.jpg" className="img-responsive" alt="..." />
+      <div className="slide-content">
+        <h3>Bowling is fun in your SPARE time.</h3>
+        <h4>Bowling is a sport for people who have talent to spare.</h4>
+      </div>
     </div>
   </Slider>
 );
